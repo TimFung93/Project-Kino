8a709c297ac86b81c85f7a16d34293b42d56139d
TimFung93 <timothy.fung.04@gmail.com>
2017-02-12 00:07:58 -0500

    static folder

---
+++ static/js/app.js
@@ -0,0 +1,26 @@
+$( document ).ready(function() {
+    
+    $('#title').click(function() {
+      $("#targetTitle1").html(title[0])
+    })
+
+
+     $('#title2').click(function() {
+    	
+      $("#targetTitle2").html(title[1])
+    })
+
+       $('#title2').click(function() {
+  
+      $("#targetTitle3").html(title[2])
+    })
+
+
+
+
+
+});
+
+
+const title = ["Pryx","Bloodknight", "Tracer", "Dragon"]
+
